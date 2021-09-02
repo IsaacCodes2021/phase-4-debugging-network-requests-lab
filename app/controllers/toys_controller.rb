fc92310477200a5bdf27d7938e38985001629186
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-09-02 15:56:00 -0700

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -6,14 +6,20 @@ class ToysController < ApplicationController
     render json: toys
   end
 
+  def show
+    toy = Toy.find_by(id: params[:id])
+    render json: toy
+  end
+
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
@@ -25,7 +31,7 @@ class ToysController < ApplicationController
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:id, :name, :image, :likes)
   end
 
 end
